b39816c8504c0475e33ab4e38c5c4d31d035570d
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-11-17 04:19:50 +0200

    Add rds cluster

---
+++ rds-cluster.tf
@@ -0,0 +1,35 @@
+resource "aws_rds_cluster" "mysql_cluster" {
+  cluster_identifier                  = "vpc-api-lambda-mysql-cluster"
+  engine                              = "aurora-mysql"
+  engine_version                      = "8.0.mysql_aurora.3.04.3"
+  database_name                       = "mydb"
+  master_username                     = variable.rds_cluster_username
+  master_password                     = variable.rds_cluster_password
+  backup_retention_period             = 5
+  preferred_backup_window             = "07:00-09:00"
+  skip_final_snapshot                 = true
+  storage_encrypted                   = true
+  apply_immediately                   = true
+  iam_database_authentication_enabled = true
+  vpc_security_group_ids              = [aws_security_group.rds_proxy_security_group.id]
+
+  tags = {
+    Name = "mysql-cluster"
+  }
+}
+
+/* add a rds instance cluster to the cluster*/
+
+resource "aws_rds_cluster_instance" "mysql_instance" {
+  count              = 2
+  cluster_identifier = aws_rds_cluster.mysql_cluster.id
+  instance_class     = "db.t3.medium"
+  engine             = "aurora-mysql"
+  identifier         = "vpc-api-lambda-mysql-cluster-instance-${count.index}"
+  apply_immediately  = true
+
+  tags = {
+    Name    = "Private VPC Course Demo"
+    Project = "AWS Terraform Course"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "mysql_cluster_username" {
+  type        = string
+  description = "mysql cluster username"
+}
+
+variable "mysql_cluster_password" {
+  type        = string
+  description = "mysql cluster password"
+}
